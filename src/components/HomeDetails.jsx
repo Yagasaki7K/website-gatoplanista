f2950ed4fec0fdbdc0cf3b600c76eaf016d6b22f
Anderson Marlon <23272064+Yagasaki7K@users.noreply.github.com>
2022-04-28 13:22:10 -0300

    Fix responsivity

---
+++ src/components/HomeDetails.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const HomeDetails = styled.div`
+
+h1 {
+    margin: 20px 0 0 0;
+}
+
+h2 {
+    margin: 0px 0 5px 0;
+}
+
+a {
+    color: #fff;
+}
+
+p {
+    justify-content: center;
+    font-size: 16px;
+    width: 50rem;
+}
+
+div {
+    margin-bottom: 20px;
+}
+
+@media screen and (max-width: 1080px) {
+    p {
+        width: 18rem;
+    }
+}
+
+`
+
+export default HomeDetails
