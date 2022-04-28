e470da35e8f875601a540fbc092633e0b0570ec6
Anderson Marlon <23272064+Yagasaki7K@users.noreply.github.com>
2022-04-28 12:48:03 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>🐱🌎</h1>
+                <h2>Você sabe o que é Gatoplanismo?</h2>
+                <strong>Junte-se a nós! ✊</strong>
+                <p>
+                    Um dos registros mais antigos descoberto do Gatoplanismo se dá em Setembro de 2017 
+                    no <a href="https://twitter.com/elordenmundial/status/913815673348001792?lang=de">Twitter</a>, 
+                    nele, é relatado que o nosso planeta tem o formato de um gato brincando com uma bolinha (Austrália)
+                    e acreditamos nisso de todas as formas. Depois da separação da Pangéia, o nosso planeta
+                    teve diversas modificações até se transformar no que é hoje e concordamos, 
+                    em montar nossa comunidade, os defensores desse ideal, que se denominam, Gatoplanista.
+                </p>
+
+                <p>
+                    O Gatoplanismo é um ideal que pode sim, acreditar tanto na ideia do Terraplanismo como na ideia
+                    de que a Terra é esférica, já que esse tipo de ideologia não fere, o Gatoplanismo, 
+                    pois independente do formato de nosso planeta, a forma do gato ainda estará presente.
+                </p>
+                <p>
+                    Sendo um Gatoplanista consequentemente você pode ser Terraplanista ou acreditar que ela é
+                    redonda, isso não interfere em nada em nosso idealismo.
+                </p>
+                <p>O gatoplanismo foi oficialmente datado como um idealismo em 27 de Abril de 2022 por Anderson
+                    "Yagasaki" Marlon, fundador do projeto, o mesmo acredita que a Terra é redonda.
+                </p>
+                <a href="https://twitter.com/Yagasaki7k">Twitter do Fundador</a>
+            </header>
+        </div>
+    )
+}
+
+export default App
